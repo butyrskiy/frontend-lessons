e9ac99229385daf6ffb14a310d6c67f3ccb5f989
potatofreedom <gustovsson6@gmail.com>
2024-01-21 23:43:34 +0700

    Add solution to practice-this

---
+++ practice-this/practice-this.js
@@ -0,0 +1,39 @@
+const obj = {
+    str: '',
+    setStr: function(input) {
+        if (!input) {
+            this.str = '';
+        } else if (typeof input === 'number') {
+            this.str = input.toString();
+        } else {
+            this.str = input;
+        }
+        return this;
+    },
+getStr: function() {
+    return this.str;
+},
+getLengthStr: function() {
+    return this.str.length;
+},
+getReverseStr: function() {
+let reverseStr = '';
+for(let i = this.str.length - 1; i >= 0; i--) {
+    reverseStr += this.str[i];
+}
+return reverseStr;
+},
+upperStr: function() {  
+    return this.str.toUpperCase();  
+}
+};
+
+function toUpperCase() {
+    return this.toUpperCase();
+}
+
+obj.setStr('Danilo');
+console.log(obj.getStr()); 
+console.log(obj.getLengthStr()); 
+console.log(obj.getReverseStr());
+console.log(toUpperCase.call(obj.getStr()));
